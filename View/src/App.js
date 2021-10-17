32f2eda1a1c0e1bbaf5a90dd0150fe644fad301e
Prince-Hamza <princehamzi.mine@gmal.com>
2021-10-17 22:36:16 +0500

    view update

--- View/src/App.js
+++ View/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Main from './screens/Main/Main'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+        <Route exact path="/" component = {Main}  />
+          {/*   <Route  exact path="/Profile/:id"  render = {(props)=>  <Profile {...props} /> } />   */}
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
